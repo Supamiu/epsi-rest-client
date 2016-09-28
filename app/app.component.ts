e81e75d85edb1ac04671c46415c390b8555dad72
Supamiu <contact@flavien-normand.fr>
2016-09-28 15:05:08 +0200

    Initial commit.

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: 'Client REST'
+})
+export class AppComponent { }
---
+++ app/app.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from "@angular/core";
+import {HttpModule} from "@angular/http";
+import {BrowserModule} from "@angular/platform-browser";
+import {AppComponent} from "./app.component";
+import {CVService} from "./service/cv.service";
+import {ProfileComponent} from "./component/profile.component";
+
+@NgModule({
+    imports: [BrowserModule, HttpModule],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent],
+    providers: []
+})
+export class AppModule {
+}
